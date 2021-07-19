0109d570de9ed56176a6dab8fb205cf6a609c3da
Anupam Tirkey <anupam.deep01@gmail.com>
2021-07-19 23:42:42 +0530

    base navbar

--- src/App.js
+++ src/App.js
@@ -3,18 +3,7 @@ import "./scss/app.scss";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>goodbye</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Navbar />
-      </header>
+      <Navbar />
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,30 @@
 import "../scss/app.scss";
-
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
 class Navbar extends Component {
-  state = {};
   render() {
-    return <div className="navbar">hello</div>;
+    return (
+      <header className="h-nav">
+        <HamMenu />
+        <div className="navbar">
+          <NavLink to="/how-we-work">How We Work</NavLink>
+          <NavLink to="/blog">Blog</NavLink>
+          <NavLink to="/account">Account</NavLink>
+          <button className="navbar-btn">View Plans</button>
+        </div>
+      </header>
+    );
   }
 }
 
 export default Navbar;
+function HamMenu() {
+  return (
+    <div className="hammenu">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </div>
+  );
+}
